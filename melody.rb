da3b4da647cf6b4b386df559be425fbf59f43d03
mohamedbadri <56170819+mohamedbadri@users.noreply.github.com>
2020-01-24 12:16:06 -0800

    Add files via upload

---
+++ melody.rb
@@ -0,0 +1,28 @@
+# Welcome to Sonic Pi v3.1
+live_loop :fbvg do
+  
+  2.times do
+    play :f4
+    sleep 0.5
+    play :f4
+    sleep 0.5
+    play :f4
+    sleep 0.5
+    play :a4
+    sleep 0.5
+  end
+  2.times do
+    play :d5
+    sleep 0.25
+    play :d5
+    sleep 0.25
+    play :d5
+    sleep 0.25
+    play :a5
+    sleep 0.25
+  end
+end
+
+
+
+
